2bc078aed94782a107b5a77e8493aef49e5d97c8
cr15h <crishsarthak@gmail.com>
2023-09-14 14:59:19 +0530

    added annotation

--- src/main/java/com/example/demo/entity/EmployeeEntity.java
+++ src/main/java/com/example/demo/entity/EmployeeEntity.java
@@ -1,86 +1,28 @@
 package com.example.demo.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.Date;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "employee")
 public class EmployeeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int id;
-    @Column(name="salary")
+    @Column(name = "salary")
     private int salary;
-    @Column(name="name")
+    @Column(name = "name")
     private String name;
-    @Column(name="mobile")
+    @Column(name = "mobile")
     private String mobileNumber;
     @Column(name = "department")
     private String departmentName;
     @Column(name = "dob")
     private Date dob;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getSalary() {
-        return salary;
-    }
-
-    public void setSalary(int salary) {
-        this.salary = salary;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getMobileNumber() {
-        return mobileNumber;
-    }
-
-    public void setMobileNumber(String mobileNumber) {
-        this.mobileNumber = mobileNumber;
-    }
-
-    public String getDepartmentName() {
-        return departmentName;
-    }
-
-    public void setDepartmentName(String departmentName) {
-        this.departmentName = departmentName;
-    }
-
-    public Date getDob() {
-        return dob;
-    }
-
-    public void setDob(Date dob) {
-        this.dob = dob;
-    }
-
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "id=" + id +
-                ", salary=" + salary +
-                ", name='" + name + '\'' +
-                ", mobileNumber='" + mobileNumber + '\'' +
-                ", departmentName='" + departmentName + '\'' +
-                ", dob=" + dob +
-                '}';
-    }
 }
-
-// id, name,mobile,department,salary,dob
-// int,varchar,varchar,varchar,int,date
